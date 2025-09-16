290f6dd3f945a891bc5502d51b9f009ef3066c5f
gaberashko <gabrielrashko02@gmail.com>
2025-09-15 23:01:32 -0400

    Sticky nav fixes

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,12 +2,15 @@ import "../scss/main.scss";
 import { Sakura } from "../models/sakura";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const sakura = new Sakura(document.body.querySelector("header")!, {
-        spawnInterval: 400,
-        maxPetals: 70,
-        windStrength: 150,
-        windChance: 0.7, // more gusts
-    });
+    const sakura = new Sakura(
+        document.body.querySelector(".body__container") as HTMLElement,
+        {
+            spawnInterval: 400,
+            maxPetals: 70,
+            windStrength: 150,
+            windChance: 0.7, // more gusts
+        }
+    );
 
     sakura.start();
 });
